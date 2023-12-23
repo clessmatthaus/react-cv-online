6e208dc0e4d944bed229a62c4dbffd23a979034a
cless matthaus <cless91@live.fr>
2023-12-23 11:02:23 +0100

    add Apropos

---
+++ src/pages/Apropos.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigator from '../components/Navigator'
+import AboutContents from '../components/AboutContents'
+import Footer from '../components/Footer'
+
+function Apropos() {
+  return (
+    <div className="apropos">
+        <Navigator/>
+        <AboutContents/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Apropos
